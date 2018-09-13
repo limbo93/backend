2f0dd3082d936aa39c08ecef65000e0f7d64775e
Mehedi Hasan <mehedi@mislbd.com>
2018-09-13 17:16:54 +0600

    endpoint registered

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 let express = require('express');
+let cors = require('cors');
+let bodyParser = require('body-parser');
 
 let app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', (req,res)=>{
-    res.send('Hello');
+let data = [
+    { message: 'R&D' },
+    { description: '...' }
+]
+
+app.get('/rnd', (req, res) => {
+    res.send(data);
+});
+
+app.post('/register', (req, res) => {
+    let userData=req.body;
+    res.sendStatus(200);
 });
 
 app.listen(3000);
