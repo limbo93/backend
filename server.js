bbec769b7de313ab417e8474b028402216c5fe35
Mehedi Hasan <mehedi@mislbd.com>
2018-09-23 19:26:36 +0600

    jwt added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let cors = require('cors');
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 let User = require('./models/User.js');
+let jwt = require('jwt-simple');
 
 let app = express();
 app.use(cors());
@@ -27,6 +28,18 @@ app.post('/register', (req, res) => {
 
         res.sendStatus(200);
     });
+});
+
+app.post('/login', async (req, res) => {
+    let userData = req.body;
+    let user = await User.findOne({ email: userData.email });
+    if (!user) return res.status(401).send({ message: 'Email or Password invalid.' });
+    if (userData.pwd !== user.password) return res.status(401).send({ message: 'Email or Password invalid.' });
+
+    let payload = {};
+    let token = jwt.encode(payload, '123');
+
+    res.status(200).send({ token });
 });
 
 mongoose.connect('mongodb://limbo:mongo_db_1@ds211613.mlab.com:11613/pssocial', { useNewUrlParser: true }, (err) => {
