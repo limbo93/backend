e9115a1e5cc35066f4d132ec80fb13b1b7dc725a
Mehedi Hasan <mehedi@mislbd.com>
2018-09-24 12:55:20 +0600

    bcrypt added

--- models/User.js
+++ models/User.js
@@ -1,6 +1,24 @@
 let mongoose = require('mongoose');
+let bcrypt = require('bcrypt-nodejs');
 
-module.exports = mongoose.model('User', {
+let userSchema = new mongoose.Schema({
     email: String,
-    pwd: String
-})
+    pwd: String,
+    name: String,
+    description: String
+});
+
+userSchema.pre('save', function (next) {
+    let user = this;
+    if (!user.isModified('pwd'))
+        return next();
+
+    bcrypt.hash(user.pwd, null, null, (err, hash) => {
+        if (err) return next(err);
+
+        user.pwd = hash;
+        next();
+    });
+});
+
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 let User = require('./models/User.js');
 let jwt = require('jwt-simple');
+let bcrypt = require('bcrypt-nodejs');
+
+mongoose.Promise = Promise;
 
 let app = express();
 app.use(cors());
@@ -18,6 +21,26 @@ app.get('/rnd', (req, res) => {
     res.send(data);
 });
 
+app.get('/users', async (req, res) => {
+    try {
+        let users = await User.find({}, '-pwd -__v');
+        res.send(users);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+});
+
+app.get('/profile/:profileId', async (req, res) => {
+    try {
+        let user = await User.findById(req.params.profileId, '-pwd -__v');
+        res.send(user);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(200);
+    }
+});
+
 app.post('/register', (req, res) => {
     let userData = req.body;
     let user = new User(userData);
@@ -26,20 +49,24 @@ app.post('/register', (req, res) => {
         if (err)
             console.log('error occured while saving user data');
 
-        res.sendStatus(200);
+        res.status(201).send(result);
     });
 });
 
 app.post('/login', async (req, res) => {
-    let userData = req.body;
-    let user = await User.findOne({ email: userData.email });
+    let loginData = req.body;
+    let user = await User.findOne({ email: loginData.email });
     if (!user) return res.status(401).send({ message: 'Email or Password invalid.' });
-    if (userData.pwd !== user.password) return res.status(401).send({ message: 'Email or Password invalid.' });
 
-    let payload = {};
-    let token = jwt.encode(payload, '123');
+    bcrypt.compare(loginData.pwd, user.pwd, (err, isMatch) => {
+        if (!isMatch)
+            return res.status(401).send({ message: 'Email or Password invalid.' });
 
-    res.status(200).send({ token });
+        let payload = {};
+        let token = jwt.encode(payload, '123');
+
+        res.status(200).send({ token });
+    });
 });
 
 mongoose.connect('mongodb://limbo:mongo_db_1@ds211613.mlab.com:11613/pssocial', { useNewUrlParser: true }, (err) => {
